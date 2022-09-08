ac83e9ae3dd62ffdc6a279c02077c0d9eac60760
Queche120 <josebustos367@gmail.com>
2022-09-08 17:38:04 -0300

    nuevos cambios

--- code.js
+++ code.js
@@ -35,3 +35,13 @@ const contenedor2 = document.querySelector(".contenedor-2");
 boton.addEventListener("click", (e)=>{
 	alert("estamos trabajando en esto.");
 });
+
+const scrollProgress = document.getElementById('scroll-progress');
+const height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', () => {
+  const scrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
